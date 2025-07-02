6cef25cb2e710312af6a485989d3c2e31d4dcf68
philipvalue <philipp1mogul@gmail.com>
2025-07-02 22:34:17 +0300

    Accepting User Inputs

---
+++ userinputs.py
@@ -0,0 +1,15 @@
+print("Enter your name:")
+name = input()
+print("Hello, " + name + "!")
+
+print("Enter your age:")
+age = input()
+print("You are " + age + " years old.")
+
+print("Enter your favorite programming language:")
+language = input()
+print("Your favorite programming language is " + language + ".")
+
+print("Enter your favorite framework:")
+framework = input()
+print("Your favorite framework is " + framework + ".")
